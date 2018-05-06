5fd3d374a2c60d853ff9d41c167639c4e23a2246
DrazenHizakowski <drazen.hizak@gmail.com>
2018-05-06 17:11:01 -0100

    Communication worksgit add avioserver/hardware_modules/motor_communicator.py !

--- avioserver/hardware_modules/motor_communicator.py
+++ avioserver/hardware_modules/motor_communicator.py
@@ -4,36 +4,65 @@ import Adafruit_PCA9685
 
 class MotorController(object):
     pwm = None
-    aerlon_channel = 0
-    aerlon_opposite_channel = 1
 
-    # Configure min and max servo pulse lengths
-    servo_min = 150  # Min pulse length out of 4096
-    servo_max = 600  # Max pulse length out of 4096
+    aerlon_channel_1 = 0
+    aerlon_channel_2 = 1 #always opposite
 
-    aerlon_max = 3000
+    elevator_channel = 2 # motor benc
+
+    gas_channel = 3
+
+    kormilo_channel = 4
+
+    flaps_channel = 5
+
+    parachute_channel = 6
+
+    aerlon_channel_1_min = 150 #for trimming
+    aerlon_channel_1_max = 500 #for trimming
+    aerlon_channel_1_zero = 300 #for trimming
+
+    aerlon_channel_2_min = 150 #for trimming
+    aerlon_channel_2_max = 500 #for trimming
+    aerlon_channel_2_zero = 300
 
     def __init__(self):
         try:
             self.pwm = Adafruit_PCA9685.PCA9685()
             self.pwm.set_pwm_freq(60)
-        except RuntimeError:
+            self.pwm.set_pwm(1,0,50) #to zero
+            self.pwm.set_pwm(0,0,50) #to zero
+        except RuntimeError as err:
+            print(err)
             self.pwm = None
+            
+        
+    def _map(self, value, istart, istop, ostart, ostop):
+        return int(ostart + (ostop - ostart) * ((value - istart) / (istop-istart)))
 
     def set_servo_pulse(self, channel, pulse):
-        pulse_length = 1000000  # 1,000,000 us per second
-
-        pulse_length //= 60  # 60 Hz
+        pulse_length = 1000000    # 1,000,000 us per second
+        pulse_length //= 60       # 60 Hz
         print('{0}us per period'.format(pulse_length))
-        pulse_length //= 4096  # 12 bits of resolution
+        pulse_length //= 4096     # 12 bits of resolution
         print('{0}us per bit'.format(pulse_length))
         pulse *= 1000
         pulse //= pulse_length
+        
         if self.pwm:
             self.pwm.set_pwm(channel, 0, pulse)
 
     def set_aerlon(self, position):
         if self.pwm:
-            self.pwm.set_pwm(self.aerlon_channel, 0, position)
-            self.pwm.set_pwm(self.aerlon_opposite_channel, 0, self.aerlon_max - position)
+            print(position)
+            self.pwm.set_pwm(
+                self.aerlon_channel_1,
+                0,
+                position
+            )
+            self.pwm.set_pwm(
+                self.aerlon_channel_2,
+                0,
+                self.aerlon_channel_2_max-position
+            )
 
--- avioserver/hardware_modules/motors.py
+++ avioserver/hardware_modules/motors.py
@@ -1,45 +1,75 @@
+# Simple demo of of the PCA9685 PWM servo/LED controller library.
+# This will move channel 0 from min to max position repeatedly.
+# Author: Tony DiCola
+# License: Public Domain
 from __future__ import division
 import time
+
+# Import the PCA9685 module.
 import Adafruit_PCA9685
 
+
+# Uncomment to enable debug output.
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
 pwm = Adafruit_PCA9685.PCA9685()
+#pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
 
-aerlon_channel = 0
-aerlon_opposite_channel = 1
+# servo motors and channels
 
-# Configure min and max servo pulse lengths
-servo_min = 150  # Min pulse length out of 4096
-servo_max = 600  # Max pulse length out of 4096
+aerlon_channel_1 = 0
+aerlon_channel_2 = 1 //always opposite
 
-aerlon_max = 3000
+elevator_channel = 2 // motor benc
 
+gas_channel = 3
+
+kormilo_channel = 4
+
+flaps_channel = 5
+
+parachute_channel = 6; 
+
+aerlon_channel_1_min = 150; //for trimming
+aerlon_channel_1_max = 500; //for trimming
+aerlon_channel_1_zero = 300; //for trimming
+
+aerlon_channel_2_min = 150; //for trimming
+aerlon_channel_2_max = 500; //for trimming
+aerlon_channel_2_zero = 300;
 
 # Helper function to make setting a servo pulse width simpler.
 def set_servo_pulse(channel, pulse):
-    pulse_length = 1000000  # 1,000,000 us per second
-    pulse_length //= 60  # 60 Hz
+    pulse_length = 1000000    # 1,000,000 us per second
+    pulse_length //= 60       # 60 Hz
     print('{0}us per period'.format(pulse_length))
-    pulse_length //= 4096  # 12 bits of resolution
+    pulse_length //= 4096     # 12 bits of resolution
     print('{0}us per bit'.format(pulse_length))
     pulse *= 1000
     pulse //= pulse_length
     pwm.set_pwm(channel, 0, pulse)
+    
+def _map(value,istart,istop,ostart,ostop):
+    return ostart + (ostop - ostart) * ((value - istart) / (istop-istart))
 
-
+#zero is 127
 def set_aerlon(position):
-    pwm.set_pwm(aerlon_channel, 0, position)
-    pwm.set_pwm(aerlon_opposite_channel, 0, aerlon_max - position)
-
-
+    pwm.set_pwm(aerlon_channel_1,0, _map(position,0,255,aerlon_channel_1_min,aerlon_channel_1_max))
+    pwm.set_pwm(aerlon_channel_1,0, _map(position,255,0,aerlon_channel_1_min,aerlon_channel_1_max))
+	
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
-
+#set_aerlon(0)
+#set_aerlon(0)
+time.sleep(1)
+pwm.set_pwm(1,0,50) #to zero
+pwm.set_pwm(0,0,50) #to zero
 print('Moving servo on channel 0, press Ctrl-C to quit...')
 while True:
     # Move servo on channel O between extremes.
-    pwm.set_pwm(1, 0, servo_min)
-    pwm.set_pwm(0, 0, servo_min)
+    #set_aerlon(500)
+    pwm.set_pwm(1,0,aerlon_channel_1_min)
     time.sleep(1)
-    pwm.set_pwm(1, 0, servo_max)
-    pwm.set_pwm(0, 0, servo_max)
+    pwm.set_pwm(1,0,aerlon_channel_1_max)
     time.sleep(1)
